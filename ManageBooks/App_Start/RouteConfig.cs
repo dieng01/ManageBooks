e4ee56ffe00dd2a7ae555a893d4ec59fcbae7622
trần công duy <trancongduy28031997@gmail.com>
2019-03-20 14:01:37 +0700

    cong cong

--- ManageBooks/App_Start/RouteConfig.cs
+++ ManageBooks/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ManageBooks
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ViewController", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ManageBooks/Controllers/ViewControllerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManageBooks.Controllers
+{
+    public class ViewControllerController : Controller
+    {
+        // GET: ViewController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
