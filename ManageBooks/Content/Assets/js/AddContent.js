2193db8a993612e3c8ba763f0225fd360f2c3f2b
trần công duy <trancongduy28031997@gmail.com>
2019-03-22 09:03:11 +0700

    1

--- ManageBooks/Content/Assets/js/AddContent.js
+++ ManageBooks/Content/Assets/js/AddContent.js
@@ -1,57 +1,48 @@
 ﻿$(document).ready(function () {
-    $("#submit").click(function (event) {
-        var Title = document.getElementById('title').value;
-        var Brief = document.getElementById('brief').value;
-        var Content = document.getElementById('content').value;
-        if (!CheckTitle(Title)) {
-            return;
-        }
-        if (!CheckBrief(Brief)) {
-            return;
-        }
-        if (!Checkcontent(Content)) {
-            alert("The content is incorrect!");
-            return;
+    $("#register-form").validate({
+        onfocusout: false,
+        onkeyup: false,
+        onclick: false,
+        rules: {
+            "username": {
+                required: true,
+                maxlength: 50,
+                minlength: 3
+            },
+            "email": {
+                required: true,
+                maxlength: 30,
+                minlength: 5
+            },
+            "password": {
+                required: true,
+                minlength: 8,
+                maxlenght: 50
+            },
+            "repassword": {
+                required: true,
+            },
+        },
+        messages: {
+            "username": {
+                required: "User Name is required",
+                maxlength: "The User Name field is too long",
+                minlength: ""
+            },
+            "email": {
+                required: "Email is required",
+                maxlength: "The Email field is too long",
+                minlength: ""
+            },
+            "password": {
+                required: "Passwword is required",
+                minlength: "Password field is too short",
+                maxlenght: "Password field is too long"
+            },
+            "repassword": {
+                required: "Passwword is required",
+            },
+
         }
     });
-
-})
-function CheckTitle(title) {
-    if (title.length > 200) {
-        alert("The lenght of the Tile is too long!");
-        return false;
-    }
-    if (title.length < 10) {
-        alert("The lenght of the title is too short!");
-        return false;
-    }
-    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-    if (title.match(format)) {
-        alert("The title field is incorrect format");
-        return false;
-    }
-    return true;
-}
-function CheckBrief(brief) {
-    if (brief.length > 150) {
-        alert("The lenght of the brief is too long!");
-        return false;
-    }
-    if (brief.length < 30) {
-        alert("The lenght of the brief is too short!");
-        return false;
-    }
-    return true;
-}
-function Checkcontent(content) {
-    if (brief.length > 1000) {
-        alert("The lenght of the content is too long!");
-        return false;
-    }
-    if (brief.length < 50) {
-        alert("The lenght of the content is too short!");
-        return false;
-    }
-    return true;
-}
-
+});
